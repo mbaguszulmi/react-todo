7e563384f85214335ac2c5cd50ee4160631a9709
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-12-28 15:10:42 +0700

    feat(redux): add redux to app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './ui/App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import configureStore from './states/store';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
