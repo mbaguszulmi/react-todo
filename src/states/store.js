769f190cf71c9f18652b7265870aeb1bc3be7f8a
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-12-28 14:20:16 +0700

    feat(redux): add store

---
+++ src/states/store.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware, compose } from "redux"
+import { thunk } from "redux-thunk"
+import rootReducer from './reducers/rootReducer'
+
+export default function configureStore(initialState = {
+        todo: {
+            todos: [], 
+            isLoadingData: false
+        }, 
+        navigation: {
+            openModal: false
+        }
+    }) {
+
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+    return createStore(
+        rootReducer,
+        initialState,
+        composeEnhancers(applyMiddleware(thunk))
+    );
+}
