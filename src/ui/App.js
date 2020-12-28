91029cc527d673df851c0f802b71e94337db0b26
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-12-28 13:29:23 +0700

    feat(ui): show modal for create new item

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize'
-import { Navbar, Icon, Button } from "react-materialize"
+import { Navbar, Icon, Button, Modal, TextInput, Textarea } from "react-materialize"
 import '../assets/css/main.scss'
 import TodoItem from '../components/TodoItem'
 
 class App extends React.Component {
+  addNewItem() {
+    
+  }
+
   render() {
     return (
       <div>
@@ -46,19 +50,52 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-  
-        <Button
-          className="red"
-          floating
-          icon={<Icon>add</Icon>}
-          large
-          node="button"
-          waves="light"
-          fab="true"
-          onClick={(event) => {
-            
+
+        <Modal
+          actions={[
+            <Button flat modal="close" node="button" waves="green">Cancel</Button>,
+            <Button modal="close" node="button" waves="green">Save</Button>,
+            <Button className="red" modal="confirm" node="button" waves="light">Delete</Button>
+          ]}
+          confirm="Are you sure?"
+          bottomSheet={false}
+          fixedFooter
+          header={<TextInput
+            id="input_todo-title"
+            label="Title"
+          />}
+          id="main-modal"
+          open={false}
+          options={{
+            dismissible: true,
+            endingTop: '10%',
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            opacity: 0.5,
+            outDuration: 250,
+            preventScrolling: true,
+            startingTop: '4%'
           }}
-        />
+          trigger={<Button
+            id="main-fab"
+            className="red"
+            floating
+            icon={<Icon>add</Icon>}
+            large
+            node="button"
+            waves="light"
+            fab="true"
+            onClick={this.addNewItem}
+          />}
+        >
+          <Textarea
+            id="input_todo-details"
+            label="Describe this todo item..."
+          />
+        </Modal>
       </div>
     )
   }
