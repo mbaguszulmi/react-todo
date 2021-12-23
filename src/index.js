1f976bf31fe0b971a661b5b1d0ba5e96be6018e8
mbaguszulmi <baguszulmi101198@gmail.com>
2021-12-23 22:13:32 +0700

    fix(app): render app with appRederer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './ui/App';
+import './main-renderer';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from "react-redux";
-import configureStore from './states/store';
 
-const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+  appRender(), document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
